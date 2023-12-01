d5f373d1b08632853f7ba70c7baecc1f8fa60e43
Jordan Kay <jordan@fleuronic.com>
2023-12-01 16:56:55 -0500

    Simplify regexes

--- Sources/Years/2023/Days/Day1.swift
+++ Sources/Years/2023/Days/Day1.swift
@@ -4,7 +4,7 @@ extension Day1: Puzzle {
 	public func solution(for part: Part, given input: String) -> Int {
 		input.split(separator: "\n").reduce(0) {
 			let digits = regexes[part]!.map($1.digit)
-			return $0 + digits[1] * 10 + digits[0]
+			return $0 + digits[0] * 10 + digits[1]
 		}
 	}
 }
@@ -21,7 +21,7 @@ let digitPlusNames = [digit, "one", "two", "three", "four", "five", "six", "seve
 let regexes = Dictionary(
 	uniqueKeysWithValues: Part.allCases.map { part in
 		let string = part == .one ? digit : digitPlusNames.joined(separator: "|")
-		return (part, ["^.*(\(string))", "(\(string)).*$"].map { regex in
+		return (part, ["(\(string))", ".*(\(string))"].map { regex in
 			try! Regex<(Substring, Substring)>(regex)
 		})
 	}
