d2a45780186a9f77af5d0d8959ee8d67b2f3fb8d
Jordan Kay <jordan@fleuronic.com>
2023-12-02 18:01:33 -0500

    Use regex builder

--- Sources/Years/2023/Days/Day1.swift
+++ Sources/Years/2023/Days/Day1.swift
@@ -1,26 +1,22 @@
 extension Day1: Puzzle {
 	public func solution(for part: Part, given input: String) -> Int {
-		input.split(separator: "\n").reduce(0) {
-			let digits = regexes[part]!.map($1.digit)
+		let digit = ChoiceOf { CharacterClass(.digit) }
+		let digits = ["one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+		let regexes = Dictionary(
+			uniqueKeysWithValues: Part.allCases.map { part in
+				let regex = switch part {
+				case .one: digit
+				case .two: ChoiceOf { digit; try! Regex(digits.keys.joined(separator: "|")) }
+				}
+
+				let capture = TryCapture(regex) { digits[.init($0)] ?? .init($0) }
+				return (part, [Regex { capture }, Regex { ZeroOrMore(.any); capture }])
+			}
+		) 
+		
+		return input.split(separator: "\n").reduce(0) {
+			let digits = regexes[part]!.compactMap($1.firstMatch).map(\.output.1)
 			return $0 + digits[0] * 10 + digits[1]
 		}
 	}
 }
-
-private extension Substring {
-	func digit(matching regex: Regex<(Self, Self)>) -> Int {
-		let match = firstMatch(of: regex)!.output.1
-		return digitPlusNames.firstIndex(of: .init(match)) ?? .init(match)!
-	}
-}
-
-private let digit = "\\d"
-private let digitPlusNames = [digit, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-private let regexes = Dictionary(
-	uniqueKeysWithValues: Part.allCases.map { part in
-		let string = part == .one ? digit : digitPlusNames.joined(separator: "|")
-		return (part, ["(\(string))", ".*(\(string))"].map { regex in
-			try! Regex<(Substring, Substring)>(regex)
-		})
-	}
-) 
