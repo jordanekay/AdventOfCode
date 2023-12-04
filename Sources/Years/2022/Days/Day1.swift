519878565a492b73ed245e279ebcc9e3146ac4e5
Jordan Kay <jordanekay@mac.com>
2023-12-04 11:35:59 -0500

    Simplify Day 1, 2022

--- Sources/Years/2022/Days/Day1.swift
+++ Sources/Years/2022/Days/Day1.swift
@@ -1,23 +1,12 @@
 extension Day1: Puzzle {
 	public func solution(for part: Part, given input: String) -> Int {
-		let totals = input
-			.split(separator: "\n\n")
-			.map(\.total)
+		let totals = input.split(separator: "\n\n").map {
+			$0.split(separator: "\n").map { Int($0)! }.reduce(0, +)
+		}
 
 		return switch part {
-		case .one: totals
-			.max()!
-		case .two: totals
-			.max(count: 3)
-			.reduce(0, +)
+		case .one: totals.max()!
+		case .two: totals.max(count: 3).reduce(0, +)
 		}
-	}
-}
-
-private extension String.SubSequence {
-	var total: Int {
-		split(separator: "\n")
-			.compactMap { Int($0) }
-			.reduce(0, +)
 	}
 }
