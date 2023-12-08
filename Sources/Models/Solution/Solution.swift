3195fd9a832f3e9c5d8f903967191f16889844c0
Jordan Kay <jordan@fleuronic.com>
2023-12-08 13:35:23 -0500

    Complete Day 8, 2023

--- Sources/Models/Solution/Solution.swift
+++ Sources/Models/Solution/Solution.swift
@@ -19,6 +19,7 @@ public struct Solution: CustomStringConvertible {
 		case (2023, 4): Year2023.Day4()
 		case (2023, 6): Year2023.Day6()
 		case (2023, 7): Year2023.Day7()
+		case (2023, 8): Year2023.Day8()
 		default: throw Error.invalidDay
 		}
 
---
+++ Sources/Years/2023/Days/Day8.swift
@@ -0,0 +1,27 @@
+extension Day8: Puzzle {
+	public func solution(for part: Part, given input: String) -> Int {
+		let components = input.split(separator: "\n\n")
+		let directions = components[0].map(String.init)
+		let pairs = components[1].split(separator: "\n").map { $0.split(separator: " = ") }
+		let network = Dictionary(uniqueKeysWithValues: pairs.map { ($0[0], $0[1]) }).mapValues {
+			$0.dropFirst().dropLast().split(separator: ", ")
+		}
+		
+		func lcm(_ a: Int, _ b: Int) -> Int { a * b / gcd(a, b) }
+		func gcd(_ a: Int, _ b: Int) -> Int { b == 0 ? a : gcd(b, a % b) }		
+		let index: (Int) -> Int = { ["L", "R"].firstIndex(of: directions[$0 % directions.count])! }
+		
+		let tests: [(Substring) -> Bool] = switch part {
+		case .one: ["AAA", "ZZZ"].map { string in { $0 == string } }
+		case .two: ["A", "Z"].map { string in { $0.last == string} }
+		}
+		
+		return network.keys.filter(tests[0]).map {
+			var (step, node) = (0, $0)
+			while !tests[1](node) { node = network[node]![index(step)]; step += 1 }
+			return step
+		}.reduce(1, lcm)
+	}
+}
+
+
