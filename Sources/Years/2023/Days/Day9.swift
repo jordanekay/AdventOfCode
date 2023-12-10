d37be29bd8e19899c1313354e329f2e56dea88e5
Jordan Kay <jordan@fleuronic.com>
2023-12-10 08:58:30 -0500

    Complete Day 9, 2023

---
+++ Sources/Years/2023/Days/Day9.swift
@@ -0,0 +1,14 @@
+extension Day9: Puzzle {
+	public func solution(for part: Part, given input: String) -> Int {
+		func nextSum(_ digits: [Int]) -> Int {
+			let (sign, next) = part == .one ? (1, digits.last!) : (-1, digits.first!)
+			let digits = zip(digits.dropFirst(), digits).map { $0.0 - $0.1 }
+			return next + sign * (digits.allSatisfy { $0 == 0 } ? 0 : nextSum(digits))
+		}
+		
+		return input.split(separator: "\n").reduce(0) {
+			let digits = $1.split(separator: " ").map { Int($0)! }
+			return $0 + nextSum(digits)
+		}
+	}
+}
