1025eb5effca8252254587854228c7703874129e
Jordan Kay <jordan@fleuronic.com>
2023-12-12 12:03:21 -0500

    Simplify Day 9, 2023

--- Sources/Years/2023/Days/Day9.swift
+++ Sources/Years/2023/Days/Day9.swift
@@ -2,7 +2,7 @@ extension Day9: Puzzle {
 	public func solution(for part: Part, given input: String) -> Int {
 		func nextSum(_ digits: [Int]) -> Int {
 			let (sign, next) = part == .one ? (1, digits.last!) : (-1, digits.first!)
-			let digits = zip(digits.dropFirst(), digits).map { $0.0 - $0.1 }
+			let digits = zip(digits.dropFirst(), digits).map(-)
 			return next + sign * (Set(digits) == [0] ? 0 : nextSum(digits))
 		}
 		
